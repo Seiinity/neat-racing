ba0049e7912d2b255798b12eaa701ba7b7a34caf
Rafael Manjua <seiinity@users.noreply.github.com>
2025-12-04 13:30:07 +0000

    Add track shape detection

--- game/track.py
+++ game/track.py
@@ -9,6 +9,7 @@ from pygame.rect import Rect
 from pygame.surface import Surface
 from pytmx import TiledImageLayer, TiledObjectGroup, TiledObject, TiledMap
 from game.config import TRACK
+from shapely.geometry import Polygon, Point
 
 class Track:
 
@@ -16,22 +17,22 @@ class Track:
 
         self.tmx_data: TiledMap = pytmx.load_pygame(tmx_path)
 
-        self.width: int = self.tmx_data.width * self.tmx_data.tilewidth
-        self.height: int = self.tmx_data.height * self.tmx_data.tileheight
+        self._width: int = self.tmx_data.width * self.tmx_data.tilewidth
+        self._height: int = self.tmx_data.height * self.tmx_data.tileheight
+        self._show_checkpoints: bool = False
 
         self.background: Surface = self._load_background()
         self.checkpoints: list[Checkpoint] = self._load_checkpoints()
         self.finish_line: Rect = self._load_finish_line()
         self.start_pos: Vector2 = self._load_start_pos()
-
-        self._show_checkpoints: bool = False
+        self.shape: Polygon = self._load_shape()
 
         self._add_listeners()
 
     def _load_background(self) -> Surface:
 
         bg_layer: TiledImageLayer = self.tmx_data.layers[0]
-        return pygame.transform.scale(bg_layer.image, (self.width, self.height))
+        return pygame.transform.scale(bg_layer.image, (self._width, self._height))
 
     def _load_checkpoints(self) -> list[Checkpoint]:
 
@@ -49,6 +50,22 @@ class Track:
         object_layer: TiledObjectGroup = self.tmx_data.layers[1]
         start_pos_obj: TiledObject = next(obj for obj in object_layer if obj.name == 'start_pos')
         return Vector2(start_pos_obj.x, start_pos_obj.y)
+
+    def _load_shape(self) -> Polygon:
+
+        bounds_layer: TiledObjectGroup = self.tmx_data.layers[2]
+
+        polygons = [obj for obj in bounds_layer if hasattr(obj, 'points')]
+        outer_bound = polygons[0]
+        inner_bound = polygons[1]
+
+        outer_points = [(p.x, p.y) if hasattr(p, 'x') else p for p in outer_bound.points]
+        inner_points = [(p.x, p.y) if hasattr(p, 'x') else p for p in inner_bound.points]
+
+        outer_polygon = Polygon(outer_points)
+        inner_polygon = Polygon(inner_points)
+
+        return outer_polygon.difference(inner_polygon)
 
     def _add_listeners(self) -> None:
 
@@ -68,6 +85,10 @@ class Track:
 
         self._show_checkpoints = not self._show_checkpoints
 
+    def is_on_track(self, position: Vector2) -> bool:
+
+        point = Point(position.x, position.y)
+        return self.shape.contains(point)
 
 class Checkpoint:
 
